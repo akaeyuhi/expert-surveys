7c2d9717ec6ffd227824860c44a581b2eb28e926
NeptunePurpleHeart <anna.bekker2401@gmail.com>
2021-12-16 04:24:35 +0200

    basic server code

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,15 @@
 "use strict";
-const models = require('./models');
-const sequelize = require('./connection');
+// const models = require('./models');
+// const sequelize = require('./connection');
+const express = require('express');
+const { router } = require('./router');
 
-(async () => {
-    await sequelize.authenticate();
-    const users = await models.User.findAll();
-    const experts = await models.Expert.findAll();
-    const answers = await models.Answer.findAll();
-    users.map(user => console.log(`${user.username}, ${user.mail}`));
-    experts.map(expert => console.log(`${expert.job}, ${expert.User_id}`));
-    answers.map(answer => console.log(`${answer.text}, ${answer.date}, ${answer.SelectedAlt_id},`))
-})();
+const app = express();
+
+app.use(express.json());
+app.use('/api', router);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
