09b087dbb9fdcf6af662c544d0fedfa31718a6c6
NeptunePurpleHeart <anna.bekker2401@gmail.com>
2021-12-16 20:24:47 +0200

    Add putAction

--- src/js/router.js
+++ src/js/router.js
@@ -2,12 +2,13 @@
 
 const express = require('express');
 const router = express.Router();
-const { getRouter } =require('./routes/getRouter');
-const { postRouter } =require('./routes/postRouter');
+const { getRouter } = require('./routes/getRouter');
+const { postRouter } = require('./routes/postRouter');
+const { putRouter } = require('./routes/putRouter');
 
 router.use('/get', getRouter);
 router.use('/post', postRouter);
-
+router.use('/put', putRouter);
 
 router.get('/', (req, res) => {
     res.send('<h1>Api root</h1>');
--- src/js/routes/putRouter.js
+++ src/js/routes/putRouter.js
@@ -12,7 +12,28 @@ putRouter.put('/survey', async (req, res) => {
         }))
         return;
     }
-    const survey = models.Survey.findByPk(req.body.surveyId);
+    const survey = await models.Survey.findByPk(req.body.surveyId);
     await survey.update(req.body.updateData);
     res.send('{response: 1}')
 });
+
+putRouter.put('/updateStatus', async (req, res) => {
+    if(!req.body.updateData) {
+        res.send(JSON.stringify({
+            error: "updateData is empty. Bad Request",
+            code: "400"
+        }))
+        return;
+    }
+    const status = await models.SurveyAction.findOne({
+        where: {
+            Survey_id: req.body.surveyId
+        }
+    });
+    await status.update(req.body.updateData);
+    res.send('{response: 1}')
+});
+
+module.exports = {
+    putRouter
+};
