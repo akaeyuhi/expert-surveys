010b23ef9a72e4916fa977700f64d3d7ed5f71e7
NeptunePurpleHeart <anna.bekker2401@gmail.com>
2021-12-16 23:53:36 +0200

    Put handling

--- src/js/src/routes/putRouter.js
+++ src/js/src/routes/putRouter.js
@@ -2,36 +2,50 @@
 
 const express = require('express');
 const putRouter = express.Router();
-const { models } = require('../models');
+const {models} = require('../models');
 
 putRouter.put('/updateSurvey', async (req, res) => {
-    if(!req.body.updateData) {
+    if (!req.body.updateData) {
         res.send(JSON.stringify({
             error: "updateData is empty. Bad Request",
             code: "400"
         }))
         return;
     }
-    const survey = await models.Survey.findByPk(req.body.surveyId);
-    await survey.update(req.body.updateData);
-    res.send('{response: 1}')
+    try {
+        const survey = await models.Survey.findByPk(req.body.surveyId);
+        await survey.update(req.body.updateData);
+        res.send('{response: 1}');
+    } catch (e) {
+        res.send({
+            error: e.message,
+            code: 400
+        })
+    }
 });
 
 putRouter.put('/updateStatus', async (req, res) => {
-    if(!req.body.updateData) {
+    if (!req.body.updateData) {
         res.send(JSON.stringify({
             error: "updateData is empty. Bad Request",
             code: "400"
         }))
         return;
     }
-    const status = await models.SurveyAction.findOne({
-        where: {
-            Survey_id: req.body.surveyId
-        }
-    });
-    await status.update(req.body.updateData);
-    res.send('{response: 1}')
+    try {
+        const status = await models.SurveyAction.findOne({
+            where: {
+                Survey_id: req.body.surveyId
+            }
+        });
+        await status.update(req.body.updateData);
+        res.send('{response: 1}');
+    } catch (e) {
+        res.send({
+            error: e.message,
+            code: 400
+        })
+    }
 });
 
 module.exports = {
